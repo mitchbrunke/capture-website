ed174bd5633bb9ced6886ba17692233d5405965b
Mitch <mitchellbrunke@gmail.com>
2021-04-13 20:34:28 +1000

    'about html done'

---
+++ src/components/FaqSection.js
@@ -0,0 +1,54 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad alias
+            sequi rem culpa. Cum vel dolor voluptate eum eveniet.
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad alias
+            sequi rem culpa. Cum vel dolor voluptate eum eveniet.
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad alias
+            sequi rem culpa. Cum vel dolor voluptate eum eveniet.
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>What Products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad alias
+            sequi rem culpa. Cum vel dolor voluptate eum eveniet.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
