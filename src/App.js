1d3d27df3413bd9c4aac05c947b23a10012c0df9
Mitch Brunke <mbrunke@fcte.com.au>
2021-04-16 16:54:10 +1000

    friday

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
+
+//components
 import Nav from "./components/Nav";
 
 //Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+
+//Router
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route exact path="/work">
+          <OurWork />
+        </Route>
+        <Route exact path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
